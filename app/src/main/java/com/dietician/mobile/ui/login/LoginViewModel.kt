dc13c544d1c0d37787abe7a1b84fcb4a82e0b144
Chandima Ranaweera <chan4lk@gmail.com>
2020-07-27 20:00:21 +0530

    add login action

--- app/src/main/java/com/dietician/mobile/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/dietician/mobile/ui/login/LoginViewModel.kt
@@ -1,6 +1,18 @@
 package com.dietician.mobile.ui.login
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import javax.inject.Inject
 
-class LoginViewModel @Inject constructor() : ViewModel()
+class LoginViewModel @Inject constructor() : ViewModel() {
+    private val _loggedIn = MutableLiveData<Boolean>().apply {
+        value = false
+    }
+    val loggedIn: LiveData<Boolean> = _loggedIn
+
+    fun login(userName: CharSequence, password: CharSequence) {
+        println(userName)
+        _loggedIn.value = true
+    }
+}
