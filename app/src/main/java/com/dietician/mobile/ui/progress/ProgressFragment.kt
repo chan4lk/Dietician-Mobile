b50d66f32db0c8e87b418bd3b7785d9f3ac53e03
Chandima Ranaweera <chan4lk@gmail.com>
2020-08-11 04:31:26 +0530

    chart added

--- app/src/main/java/com/dietician/mobile/ui/progress/ProgressFragment.kt
+++ app/src/main/java/com/dietician/mobile/ui/progress/ProgressFragment.kt
@@ -6,12 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentFactory
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.dietician.mobile.DieticianApplication
 import com.dietician.mobile.R
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import javax.inject.Inject
+
 
 class ProgressFragment : Fragment(){
 
@@ -36,7 +40,24 @@ class ProgressFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
 
-        return inflater.inflate(R.layout.fragment_progress, container,false)
+        val root = inflater.inflate(R.layout.fragment_progress, container, false)
+        val chart: LineChart = root.findViewById(R.id.chart1)
+        val dataObjects = listOf(1F, 2F, 3F, 4F)
+        val entries: MutableList<Entry> =
+            ArrayList()
+        for (data in dataObjects) {
+            entries.add(Entry(data, data * 2))
+        }
+
+        val dataSet = LineDataSet(entries, "Label")
+        dataSet.color = R.color.colorAccent
+        dataSet.valueTextColor = R.color.colorPrimary
+        val lineData = LineData(dataSet)
+        chart.data = lineData
+        chart.invalidate() // refresh
+
+
+        return root
 
     }
 }
