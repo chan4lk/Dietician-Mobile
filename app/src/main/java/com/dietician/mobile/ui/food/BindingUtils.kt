049b5a7924a3d324587e4538f25e3cce02574334
Nadeera <kodithuwakku.uom@gmail.com>
2020-09-26 00:11:38 +0530

    added food images and set them into diet menu

--- app/src/main/java/com/dietician/mobile/ui/food/BindingUtils.kt
+++ app/src/main/java/com/dietician/mobile/ui/food/BindingUtils.kt
@@ -38,14 +38,43 @@ fun TextView.setFoodFatString(item: Food?) {
 @BindingAdapter("foodImage")
 fun ImageView.setSleepImage(item: Food?) {
     item?.let {
-//        when (item.name) {
-//            "Brown Rice (cooked)" -> print("x == 1")
-//            "Egg Noodles" -> print("x == 2")
-//            else -> { // Note the block
-//                print("x is neither 1 nor 2")
-//            }
-//        }
-        setImageResource(R.drawable.food2)
+        when (item.name) {
+            "Brown Rice (cooked)" -> setImageResource(R.drawable.brown_rice)
+            "Egg Noodles" -> setImageResource(R.drawable.egg_noodles)
+            "Rice Noodles(cooked)" -> setImageResource(R.drawable.rice_noodles)
+            "Roast Chicken" -> setImageResource(R.drawable.roast_chicken)
+            "Fride Rice" -> setImageResource(R.drawable.fride_rice)
+            "Tuna Fish" -> setImageResource(R.drawable.tuna_fish)
+            "Baked Fish" -> setImageResource(R.drawable.baked_fish)
+            "Fish Sandwich" -> setImageResource(R.drawable.fish_sandwich)
+            "Vegie Soup" -> setImageResource(R.drawable.veg_soup)
+            "String Hopper" -> setImageResource(R.drawable.string_hoppers)
+            "Dhal Curry" -> setImageResource(R.drawable.dhal_curry)
+            "Salad Leaves" -> setImageResource(R.drawable.salad_leaves)
+            "Baked Patatoes" -> setImageResource(R.drawable.baked_potatos)
+            "Boiled Carrots" -> setImageResource(R.drawable.boiled_carrots)
+            "Carrot (cooked)" -> setImageResource(R.drawable.carrot_cooked)
+            "Carrot Salad" -> setImageResource(R.drawable.carrot_salad)
+            "Seafood pasta salad" -> setImageResource(R.drawable.seafood_pasta_salad)
+            "Dried Mango Slices" -> setImageResource(R.drawable.dried_mango)
+            "Avacado" -> setImageResource(R.drawable.avacado)
+            "Avacado Ice Cream" -> setImageResource(R.drawable.avacado_ice_cream)
+            "Garlic Bread" -> setImageResource(R.drawable.galic_bread)
+            "Banana Bread" -> setImageResource(R.drawable.banana_bread)
+            "Bread" -> setImageResource(R.drawable.bread)
+            "Pumkin (cooked)" -> setImageResource(R.drawable.pumkin_cooked)
+            "Beet (cooked)" -> setImageResource(R.drawable.beet)
+            "Salmon" -> setImageResource(R.drawable.salmon)
+            "Banana" -> setImageResource(R.drawable.banana)
+            "Roti" -> setImageResource(R.drawable.roti)
+            "Chocolate Cake" -> setImageResource(R.drawable.chocalate_cake)
+            "Egg & Cheese Sandwich" -> setImageResource(R.drawable.egg_cheese_sandwich)
+            "Green Beans" -> setImageResource(R.drawable.green_beans)
+            else -> { // Note the block
+                setImageResource(R.drawable.food2)
+            }
+        }
+
     }
 }
 
